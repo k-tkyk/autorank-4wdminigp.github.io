7510f102b9185130a4ff66b21a88f564ab86da1e
TKYK <konno.takeyuki@gmail.com>
2021-04-26 22:09:45 +0900

    アクセルアーマーのパワー補正値を修正

--- asset/js/mainscommon.js
+++ asset/js/mainscommon.js
@@ -104,7 +104,7 @@ function Diagnosis_Calc(resultValueKai, shindantire, shindantirekei) {
 	if (bodyOption1 == 2) bodyPower += 0.02;
 	if (bodyOption1 == 12) bodyPower += 0.03;
 	if (bodyOption1 == 22) bodyPower += 0.05;
-	if (bodyOption1 == 25) bodyPower += 0.06;
+	if (bodyOption1 == 25) bodyPower += 0.075;
 	if (bodyOption1 == 30) bodyPower += 0.03;
 	if (bodyOption1 == 31) bodyPower += 0.03;
 	if (bodyOption2 == 2) bodyPower += 0.006;
